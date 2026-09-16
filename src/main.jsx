18f31cee0f1a453939e57571a3f4bcb38372f239
echo-eng928 <echoa981@gmail.com>
2026-09-16 09:57:14 +0800

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// 1. 引入 Vercel Analytics 组件
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+    {/* 2. 渲染 Analytics */}
+    <Analytics />
+  </React.StrictMode>,
+)
